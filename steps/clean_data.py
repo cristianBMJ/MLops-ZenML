330fb660e60c2403b04c75fd00e6be4a95d94dd6
CristianMB <52616233+cristianBMJ@users.noreply.github.com>
2024-04-01 13:43:33 +0000

    Add in Steps and pipelines

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -9,7 +9,7 @@ from typing import Tuple
 @step
 def clean_df(df: pd.DataFrame) -> Tuple[
     Annotated[ pd.DataFrame, "X_train"],
-    Annotated[ pd.DataFrame, "x_test"],
+    Annotated[ pd.DataFrame, "X_test"],
     Annotated[ pd.Series, "y_train"],
     Annotated[ pd.Series, "y_test"],
 ]:
@@ -32,8 +32,10 @@ def clean_df(df: pd.DataFrame) -> Tuple[
 
         divide_strategy = DataDivideStrategy()
         data_cleaning = DataCleaning( processed_data, divide_strategy )
-        X_train, x_test, y_train, y_test = data_cleaning.handle_data()
-        logging.info( "Data Cleaning Completed"
+        X_train, X_test, y_train, y_test = data_cleaning.handle_data()
+        logging.info("Data Cleaning Completed")
+        return  X_train, X_test, y_train, y_test 
+                     
     except Exception as e:
-        logging.erro("Error in cleaning data: {}".format(e))
+        logging.error("Error in cleaning data: {}".format(e))
         raise e
--- steps/evaluation.py
+++ steps/evaluation.py
@@ -1,13 +1,43 @@
 import logging
+from typing import Tuple
+from typing_extensions import Annotated
+
 from zenml import step
 import pandas as pd
+from sklearn.base import RegressorMixin
+
+
+
+from src.evaluation import MSE ,R2, RMSE
+
+
 
 @step
-def evaluate_model( df: pd.DataFrame) -> None:
+def evaluate_model( model: RegressorMixin,
+    X_test: pd.DataFrame,
+    y_test: pd.DataFrame,
+) -> tuple[
+    Annotated[float, "r2_score"],
+    Annotated[float, "rmse"]
+    ]:  
     """
-    Evaluates the model on the ingested Data.
+            Evaluates the model on the ingested Data.
 
     Args:
         df: The ingested data
     """
-    pass
+    try:
+        prediction = model.predict( X_test)
+        mse_class = MSE()
+        mse = mse_class.calculate_scores( y_test, prediction)
+
+        r2_class = R2() 
+        r2 = r2_class.calculate_scores( y_test, prediction)
+
+        rmse_class = RMSE() 
+        rmse = rmse_class.calculate_scores( y_test, prediction)
+
+        return r2, rmse
+    except Exception as e:
+        logging.error("Error in evaluating model: {}".format(e))
+        raise e
